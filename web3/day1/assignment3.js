fc18979afe0080781ee51e985d47ed22afaa4ed1
akashkroy <akashkroy@hotmail.com>
2024-09-16 22:09:47 +0530

    work tree cleaning

--- web3/day1/assignment3.js
+++ web3/day1/assignment3.js
@@ -7,7 +7,6 @@
 
 const input = 'harkirat => Raman | Rs 100Ram => Ankit | Rs 10'
 const crypto = require('crypto')
-var hash =''
 
 function findHasWith0000(){
 
@@ -16,7 +15,7 @@ function findHasWith0000(){
 
     while(true){
         var inputStr = input + nounce.toString()
-        hash = crypto.createHash('sha256').update(inputStr).digest('hex')
+        const hash = crypto.createHash('sha256').update(inputStr).digest('hex')
         if(hash.startsWith(prefix)){
             return nounce
         }
@@ -43,3 +42,13 @@ console.log("nounce: ",findHasWith0000())
 // Nonce = 2 → Hash: B8C2...
 // Nonce = 3 → Hash: 00005ABC... (Valid)
 // Once they find a valid hash (like the one with leading zeros), the block is considered mined.
+
+// Why Use a Nonce?: The reason miners use the nonce is that hash functions 
+// produce very different outputs for even the smallest change in input. By 
+// changing the nonce, miners effectively get a new hash each time, allowing 
+// them to perform a massive number of attempts (trial and error) to find a 
+// hash that meets the network's requirements.
+
+// Block Validation: Once a miner finds a valid nonce that produces the desired hash,
+//  the block is added to the blockchain, and the miner is rewarded with cryptocurrency. 
+//  This valid block is then broadcast to the entire network, and other nodes verify it.
